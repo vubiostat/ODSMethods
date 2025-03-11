eab3a84fda1780244e9589312e389170ad1a1fab
Shawn Garbett <shawn.garbett@vumc.org>
2025-03-11 18:02:00 -0500

    Added mean method to acml tests

--- tests/testthat/test-011-acml-continuous.R
+++ tests/testthat/test-011-acml-continuous.R
@@ -32,3 +32,32 @@ test_that("ACML continuous response intercept",
   #expect_true("df.residual" %in% names(est))
 
 })
+
+test_that("ACML continuous response intercept",
+{
+  expect_silent(
+    design <- ods(response ~ month|patient,
+                  'mean',
+                  p_sample=c(1, 0.25, 1),
+                  data=data,
+                  quantiles=c(0.1, 0.9)))
+
+  expect_silent(
+    est <- acml(response ~ month*genotype, design, data, init=rep(1, 8))
+  )
+
+  expect_true(inherits(est, "acml"))
+  expect_equal(est$Code,   2) # This changes based on method
+  expect_close(coef(est),   estimates_acml_mean)
+  expect_close(est$LogL,    logl_acml_mean)
+  expect_close(vcov(est),   cv_acml_mean)
+  expect_close(robcov(est), rcv_acml_mean)
+
+  #expect_true("residuals" %in% names(est))     # There are 2 levels!
+  #expect_true("fitted.values" %in% names(est)) # There are 2 levels!
+
+  # This is really complicated
+  #expect_true("rank" %in% names(est))
+  #expect_true("df.residual" %in% names(est))
+
+})
