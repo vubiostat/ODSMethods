2fc0eee3c398e9121bf276e5c1054666c9d93dce
Shawn Garbett <shawn@garbett.org>
2025-06-23 08:31:30 -0500

    Small DRY refactor for validated tests

---
+++ tests/testthat/helper-004-call-validated.R
@@ -0,0 +1,37 @@
+
+# This function takes data and creates the design objects in the validated
+# direct manner and calls the validated reference.
+# It assumes the reference data matches the columns of gbti
+
+validated_ll <- function(data, est, method, cuts=c(0.1, 0.9), probs=c(1, 0.25, 1))
+{
+  b_i <-sapply(
+    split(data, data$Patient),
+    function(d) c(mean(d$Response),coef(lm(Response~Month, d)))
+  )
+  rownames(b_i) <- c("mean", "intercept", "slope")
+
+  cuts <- if(method == 'bivar')
+  {
+    c(
+      quantile(b_i['intercept',], probs=cuts),
+      quantile(b_i['slope',],     probs=cuts)
+    )
+  } else quantile(b_i[method,], probs=cuts)
+
+  data <- data[!is.na(data$Genotype),]  # Drop NA data for these tests
+
+  # Create the design matrices
+  d_mat <- cbind(rep(1, nrow(data)),
+                 data[,c("Month", "Genotype")])
+  d_mat$g_t <- d_mat$Month*d_mat$Genotype
+
+  y  <- matrix(data$Response,ncol = 1)
+  x  <- as.matrix(d_mat)
+  z  <- as.matrix(cbind(rep(1, length(data$Month)), data$Month))
+  id <- matrix(data$Patient,ncol = 1)
+
+  av_LogLikeAndScore(
+    est, y, x, z, id, method, cuts, c(1, 0.25, 1),
+    rep(1, length(y)), NA)
+}
--- tests/testthat/test-001-acml-validated.R
+++ tests/testthat/test-001-acml-validated.R
@@ -2,33 +2,9 @@ context("ACML Validated Continuous Likelihood Methods")
 
 data(gbti)
 
-data <- gbti
-
-b_i <-sapply(
-        split(data, data$Patient),
-        function(d) c(mean(d$Response),coef(lm(Response~Month, d)))
-      )
-rownames(b_i) <- c("mean", "intercept", "slope")
-
-data <- data[!is.na(data$Genotype),]  # Drop NA data for these tests
-
-# Create the design matrices
-d_mat <- cbind(rep(1, nrow(data)),
-               data[,c("Month", "Genotype")])
-d_mat$g_t <- d_mat$Month*d_mat$Genotype
-
-y  <- matrix(data$Response,ncol = 1)
-x  <- as.matrix(d_mat)
-z  <- as.matrix(cbind(rep(1, length(data$Month)), data$Month))
-id <- matrix(data$Patient,ncol = 1)
-
 test_that("Validation ACML 'mean' reference works",
 {
-  cuts <- quantile(b_i['mean',], probs=c(0.1, 0.9))
-
-  result <- av_LogLikeAndScore(
-    estimates_acml_mean, y, x, z, id, 'mean', cuts, c(1, 0.25, 1),
-    rep(1, length(y)), NA)
+  result <- validated_ll(gbti, estimates_acml_mean, 'mean')
 
   expect_close(result,                   logl_acml_mean)
   expect_close(attr(result, "gradient"), gradient_acml_mean)
@@ -36,11 +12,7 @@ test_that("Validation ACML 'mean' reference works",
 
 test_that("Validation ACML 'intercept' reference works",
 {
-  cuts <- quantile(b_i['intercept',], probs=c(0.1, 0.9))
-
-  result <- av_LogLikeAndScore(
-    estimates_acml_intercept, y, x, z, id, 'intercept', cuts, c(1, 0.25, 1),
-    rep(1, length(y)), NA)
+  result <- validated_ll(gbti, estimates_acml_intercept, 'intercept')
 
   expect_close(result,                   logl_acml_intercept)
   expect_close(attr(result, "gradient"), gradient_acml_intercept)
@@ -48,11 +20,7 @@ test_that("Validation ACML 'intercept' reference works",
 
 test_that("Validation ACML 'slope' reference works",
 {
-  cuts <- quantile(b_i['slope',], probs=c(0.1, 0.9))
-
-  result <- av_LogLikeAndScore(
-    estimates_acml_slope, y, x, z, id, 'slope', cuts, c(1, 0.25, 1),
-    rep(1, length(y)), NA)
+  result <- validated_ll(gbti, estimates_acml_slope, 'slope')
 
   expect_close(result,                   logl_acml_slope)
   expect_close(attr(result, "gradient"), gradient_acml_slope)
@@ -60,14 +28,7 @@ test_that("Validation ACML 'slope' reference works",
 
 test_that("Validation ACML 'bivariate' reference works",
 {
-  cuts <- c(
-    quantile(b_i['intercept',], probs=c(0.1, 0.9)),
-    quantile(b_i['slope',], probs=c(0.1, 0.9))
-  )
-
-  result <- av_LogLikeAndScore(
-    estimates_acml_bivar, y, x, z, id, 'bivar', cuts, c(1, 0.25, 1),
-    rep(1, length(y)), NA)
+  result <- validated_ll(gbti, estimates_acml_bivar, 'bivar')
 
   expect_close(result,                   logl_acml_bivar)
   expect_close(attr(result, "gradient"), gradient_acml_bivar)
